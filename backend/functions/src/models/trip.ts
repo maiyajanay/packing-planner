b154af2d476600cc297b415b7142d7596649ee41
Greg Robinson <robgreg@umich.edu>
2024-06-24 22:01:32 -0400

    connecting db

--- backend/functions/src/models/trip.ts
+++ backend/functions/src/models/trip.ts
@@ -3,4 +3,14 @@ import { ObjectId } from 'mongodb';
 export default interface Trip {
     _id?: ObjectId,
     to: string,
+    dates: string,
+    shorts: number,
+    pants: number,
+    shirts: number,
+    socks: number,
+    underwear: number,
+    sweatshirt: number,
+    jacket: number,
+    complete: boolean,
 }
+
