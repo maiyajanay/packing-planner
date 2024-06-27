bff8fcd74418b427eb1df26b58bb546f819a2a5f
Maiya Hoard <maiyajanay@gmail.com>
2024-06-26 20:02:25 -0400

    weather API start

--- frontend/src/models/trip.ts
+++ frontend/src/models/trip.ts
@@ -1,6 +1,13 @@
-import { ObjectId } from 'mongodb';
-
 export default interface Trip {
-    _id?: ObjectId,
+    _id?: string,
     to: string,
+    dates: string,
+    shorts: number,
+    pants: number,
+    shirts: number,
+    socks: number,
+    underwear: number,
+    sweatshirt: number,
+    jacket: number,
+    complete: boolean,
 }
