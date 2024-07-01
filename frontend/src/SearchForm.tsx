1d90f44a183f17fc6cf09dcab8672febcbd84ce5
Maiya Hoard <maiyajanay@gmail.com>
2024-07-01 18:11:02 -0400

    committing the search form

--- frontend/src/SearchForm.tsx
+++ frontend/src/SearchForm.tsx
@@ -1,4 +1,27 @@
-export function SearchForm() {
-    return <></>;
-  }
+import { useState } from "react";
+
+interface SearchFormProps {
+  onSearch: (term: string) => void;
+}
+
+export function SearchForm({onSearch}: SearchFormProps) {
+    const [term, setTerm] = useState<string>('');  
+  
+    function handleSubmit(e: React.FormEvent) {
+      e.preventDefault();
+      onSearch(term);
+    }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input
+          type="text"
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+          placeholder="Search Your Destination"
+      />
+      <button type="submit">Search</button>
+  </form>
+  );
+}
   
