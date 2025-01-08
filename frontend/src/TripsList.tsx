77eb375ae2308dd6a782edef04c96d911d5ec9a6
Maiya <maiyajanay@gmail.com>
2025-01-08 15:23:28 -0500

    made some styling changes, including header spacing, text margin, and added a container for the trip_cards on the home page with spacing at the bottom

--- frontend/src/TripsList.tsx
+++ frontend/src/TripsList.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { TripCard } from "./TripCard";
 import TripContext from "./tripContext/TripContext";
+import "./TripsList.css"
 
 export function TripList() {
   const { trips, handleDelete } = useContext(TripContext);
@@ -10,19 +11,23 @@ export function TripList() {
   };
 
   return (
-    <div>
-      <h2>Your Upcoming Trips</h2>
-      <div>
-        {trips
-          .filter((trip) => !trip.complete)
-          .map((trip) => (
-            <TripCard
-              key={trip._id?.toString()}
-              trip={trip}
-              OnDelete={handleSendDelete}
-              OnEdit={() => {}}
-            />
-          ))}
+    <div className="trip_list_section">
+      <div className="trip_list_container">
+        <div className="trip_list_header">
+          <h2>Your Upcoming Trips</h2>
+        </div>
+        <div className="trip_card_container">
+          {trips
+            .filter((trip) => !trip.complete)
+            .map((trip) => (
+              <TripCard
+                key={trip._id?.toString()}
+                trip={trip}
+                OnDelete={handleSendDelete}
+                OnEdit={() => {}}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
