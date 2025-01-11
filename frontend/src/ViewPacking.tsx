e87be7b57be2e301c616d0e56bda4abd3d42b898
Maiya <maiyajanay@gmail.com>
2025-01-10 23:10:37 -0500

    made some styling changes to Packing List viewing page. INCL: Adding new component for title and location, changing css styling for better UX.

--- frontend/src/ViewPacking.tsx
+++ frontend/src/ViewPacking.tsx
@@ -5,9 +5,11 @@ import TripContext from "./tripContext/TripContext";
 import "./ViewPacking.css";
 import { WeatherCard } from "./WeatherCard";
 import { WeatherTile } from "./WeatherTile";
+import { ViewPackingTitle } from "./ViewPackingTitle";
 import { Weather } from "./models/weather";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 export function ViewPacking() {
   const navigate = useNavigate();
   const { trips, fetchAndSetTrips, handleEdit } = useContext(TripContext);
@@ -39,94 +41,98 @@ export function ViewPacking() {
   }
 
   return (
-    <div className="viewPacking">
-      
-      {/* <div className="viewWeather">
-        <div>
-          <h2 id="weatherTitle">Weather</h2>
-        </div>
-        <div className="weatherReport">
-          {Array.isArray(trip.weather) ? (
-            trip.weather
-              ?.slice(0, trip.duration)
-              .map((forecast: Weather) => (
-                <WeatherCard key={forecast.Date} forecast={forecast} />
-              ))
-          ) : (
-            <WeatherCard key={0} forecast={trip.weather!} />
-          )}
-        </div>
-      </div> */}
-
-      <div className="viewWeatherTile">
-        <div>
-          <h2 id="weatherTileTitle">Weather</h2>
-        </div>
-        <div className="weatherTileReport">
-          {Array.isArray(trip.weather) ? (
-            trip.weather
-              ?.slice(0, trip.duration)
-              .map((forecast: Weather) => (
-                <WeatherTile key={forecast.Date} forecast={forecast} />
-              ))
-          ) : (
-            <WeatherTile key={0} forecast={trip.weather!} />
-          )}
-        </div>
+    <div>
+      <div className="viewPackingTitleContainer">
+        <ViewPackingTitle />
       </div>
+      <div className="viewPacking">
+          {/* <div className="viewWeather">
+          <div>
+            <h2 id="weatherTitle">Weather</h2>
+          </div>
+          <div className="weatherReport">
+            {Array.isArray(trip.weather) ? (
+              trip.weather
+                ?.slice(0, trip.duration)
+                .map((forecast: Weather) => (
+                  <WeatherCard key={forecast.Date} forecast={forecast} />
+                ))
+            ) : (
+              <WeatherCard key={0} forecast={trip.weather!} />
+            )}
+          </div>
+        </div> */}
 
-      <div className="viewList">
-        <h1 className="name">{trip?.name}</h1>
-        <h2 className="to">{trip?.to}</h2>
-
-        <div className="checklist">
-          <h3>Packing List</h3>
-          <div className="packingCheckbox">
-            <label>Shorts: {trip?.shorts}</label>
-            <input type="checkbox" readOnly checked={false} />
+        <div className="viewWeather">
+          <div>
+            <h2 id="weatherTitle">Weather</h2>
           </div>
-          <div className="packingCheckbox">
-            <label>Pants: {trip?.pants}</label>
-            <input type="checkbox" readOnly checked={false} />
-          </div>
-          <div className="packingCheckbox">
-            <label>Shirts: {trip?.shirts}</label>
-            <input type="checkbox" readOnly checked={false} />
-          </div>
-          <div className="packingCheckbox">
-            <label>Socks: {trip?.socks}</label>
-            <input type="checkbox" readOnly checked={false} />
-          </div>
-          <div className="packingCheckbox">
-            <label>Underwear: {trip?.underwear}</label>
-            <input type="checkbox" readOnly checked={false} />
-          </div>
-          <div className="packingCheckbox">
-            <label>Sweatshirt: {trip?.sweatshirt}</label>
-            <input type="checkbox" readOnly checked={false} />
-          </div>
-          <div className="packingCheckbox">
-            <label>Jacket: {trip?.jacket}</label>
-            <input type="checkbox" readOnly checked={false} />
+          <div className="weatherReport">
+            {Array.isArray(trip.weather) ? (
+              trip.weather
+                ?.slice(0, trip.duration)
+                .map((forecast: Weather) => (
+                  <WeatherTile key={forecast.Date} forecast={forecast} />
+                ))
+            ) : (
+              <WeatherTile key={0} forecast={trip.weather!} />
+            )}
           </div>
         </div>
-        {trip?.complete ? (
-          <button onClick={() => navigate("/previoustrips")}> Back </button>
-        ) : (
-          <div className="viewButtons">
-            <div /*className="editButton" <- temporarily removed the class from this spot*/> 
-              <Link className="editButton" to={`/packing/${trip?._id}`}>Edit Packing</Link>
+
+        <div className="viewList">
+          {/* <h1 className="name">{trip?.name}</h1>
+          <h2 className="to">{trip?.to}</h2> */}
+
+          <div className="checklist">
+            <h3>Packing List</h3>
+            <div className="packingCheckbox">
+              <label>Shorts: {trip?.shorts}</label>
+              <input type="checkbox" readOnly checked={false} />
             </div>
-            <div className="actionButtons">
-              <button id="back" onClick={() => navigate("/")}>
-                Back
-              </button>
-              <button id="complete" onClick={handleComplete}>
-                Trip Completed
-              </button>
+            <div className="packingCheckbox">
+              <label>Pants: {trip?.pants}</label>
+              <input type="checkbox" readOnly checked={false} />
+            </div>
+            <div className="packingCheckbox">
+              <label>Shirts: {trip?.shirts}</label>
+              <input type="checkbox" readOnly checked={false} />
+            </div>
+            <div className="packingCheckbox">
+              <label>Socks: {trip?.socks}</label>
+              <input type="checkbox" readOnly checked={false} />
+            </div>
+            <div className="packingCheckbox">
+              <label>Underwear: {trip?.underwear}</label>
+              <input type="checkbox" readOnly checked={false} />
+            </div>
+            <div className="packingCheckbox">
+              <label>Sweatshirt: {trip?.sweatshirt}</label>
+              <input type="checkbox" readOnly checked={false} />
+            </div>
+            <div className="packingCheckbox">
+              <label>Jacket: {trip?.jacket}</label>
+              <input type="checkbox" readOnly checked={false} />
             </div>
           </div>
-        )}
+          {trip?.complete ? (
+            <button onClick={() => navigate("/previoustrips")}> Back </button>
+          ) : (
+            <div className="viewButtons">
+              <div /*className="editButton" <- temporarily removed the class from this spot*/> 
+                <Link className="editButton" to={`/packing/${trip?._id}`}>Edit Packing</Link>
+              </div>
+              <div className="actionButtons">
+                <button id="back" onClick={() => navigate("/")}>
+                  Back
+                </button>
+                <button id="complete" onClick={handleComplete}>
+                  Trip Completed
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/ViewPackingTitle.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import Trip from "./models/trip";
+import { Weather } from "./models/weather";
+import { format, parseISO } from 'date-fns';
+import "./ViewPackingTitle.css";
+interface ViewPackingTitleProps {
+  trip: Trip;
+  forecast: Weather;
+}
+
+// export function ViewPackingTitle({ trip, forecast }: ViewPackingTitleProps) {
+export function ViewPackingTitle() {
+    
+    //NEED TO FIGURE OUT HOW TO ADD DATES - CAN REFERENCE OTHER COMPONENTS
+    // const getFormattedDateParts = (dateString: any) => {
+    //     const dayOfWeek = format(parseISO(dateString), 'EEEE');
+    //     const monthAndDay = format(parseISO(dateString), 'MMMM d');
+    //     return { dayOfWeek, monthAndDay };
+    // };
+
+    // const { dayOfWeek, monthAndDay } = getFormattedDateParts(forecast.Date.substring(0, 10));
+
+    return (
+        <div className="viewPackingTitle">
+            <h2>Trip Name Placeholder</h2>
+            <p>Trip Location & Duration Placeholder</p>
+            {/* <h1 className="name">{trip?.name}</h1>
+            <h2 className="to">{trip?.to}</h2> */}
+        </div>
+    );
+}
