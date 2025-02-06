82fe8a2fbd0aa1ffb6ebd4c04255f2b9c7055579
Maiya <maiyajanay@gmail.com>
2025-02-06 12:08:49 -0500

    Added MakeList component to conditionally render in the PackingList component

---
+++ frontend/src/MakeList.tsx
@@ -0,0 +1,141 @@
+import { useContext, useState } from "react";
+import Trip from "./models/trip";
+import { useNavigate, useParams } from "react-router-dom";
+import "./PackingForm.css";
+import TripContext from "./tripContext/TripContext";
+import { Weather } from "./models/weather";
+import { WeatherCard } from "./WeatherCard";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ViewPackingTitle } from "./ViewPackingTitle";
+import { WeatherTile } from "./WeatherTile";
+
+interface PackingFormProps {
+    onEdit: (trip: Trip, id: string) => void;
+}
+export function PackingForm({ onEdit }: PackingFormProps) {
+    const notify = () =>
+        toast.success("Trip Saved!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    const navigate = useNavigate();
+    const { trips } = useContext(TripContext);
+    const _id: string | undefined = useParams().id;
+    const trip: Trip = trips.find((foundtrip) => foundtrip._id === _id)!;
+
+    const [shorts, setShorts] = useState<number>(trip.shorts || 0);
+    const [pants, setPants] = useState<number>(trip.pants || 0);
+    const [shirts, setShirts] = useState<number>(trip.shirts || 0);
+    const [socks, setSocks] = useState<number>(trip.socks || 0);
+    const [underwear, setUnderwear] = useState<number>(trip.underwear || 0);
+    const [sweatshirt, setSweatshirt] = useState<number>(trip.sweatshirt || 0);
+    const [jacket, setJacket] = useState<number>(trip.jacket || 0);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        onEdit(
+        {
+            name: trip.name,
+            to: trip.to,
+            duration: trip.duration,
+            shorts: shorts,
+            pants: pants,
+            shirts: shirts,
+            socks: socks,
+            underwear: underwear,
+            sweatshirt: sweatshirt,
+            jacket: jacket,
+            weather: trip.weather,
+            complete: false,
+            open: true,
+        },
+        trip._id?.toString() || ""
+        );
+        // navigate("/");
+        // Maiya added the below navigation to stay on the packing page without having to navigate back to the home page
+        navigate(`/viewpacklist/${trip?._id}`);
+    }
+
+return (
+    <div>   
+        <form className="packingForm" onSubmit={handleSubmit}>
+            {trip.open ? (
+                <h2 id="updateTitle">Update Packing List</h2>
+                ) : (
+                <h2 id="createTitle">Create Packing List</h2>
+                )}
+            <label>
+                Shorts:
+                <input
+                type="number"
+                value={shorts}
+                onChange={(e) => setShorts(parseInt(e.target.value))}
+                />
+            </label>    
+            <label>
+                Pants:
+                <input
+                type="number"
+                value={pants || 0}
+                onChange={(e) => setPants(parseInt(e.target.value))}
+                />
+            </label>
+            <label>
+                Shirts:
+                <input
+                type="number"
+                value={shirts}
+                onChange={(e) => setShirts(parseInt(e.target.value))}
+                />
+            </label>
+            <label>
+                Socks:
+                <input
+                type="number"
+                value={socks}
+                onChange={(e) => setSocks(parseInt(e.target.value))}
+                />
+            </label>
+            <label>
+                Underwear:
+                <input
+                type="number"
+                value={underwear}
+                onChange={(e) => setUnderwear(parseInt(e.target.value))}
+                />
+            </label>
+            <label>
+                Sweatshirt:
+                <input
+                type="number"
+                value={sweatshirt}
+                onChange={(e) => setSweatshirt(parseInt(e.target.value))}
+                />
+            </label>
+            <label>
+                Jacket:
+                <input
+                type="number"
+                value={jacket}
+                onChange={(e) => setJacket(parseInt(e.target.value))}
+                />
+            </label>
+            <div className="actionButtons">
+                <button className="packingButton" id="save" type="submit" onClick={notify}>
+                Save Trip
+                </button>
+                <button className="packingButton" onClick={() => navigate("/")}>
+                Cancel
+                </button>            
+            </div>
+        </form>
+    </div>
+);
+}
